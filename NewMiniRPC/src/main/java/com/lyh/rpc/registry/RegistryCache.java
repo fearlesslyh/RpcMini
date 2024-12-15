479afad69e99fd187c5f3b941bbbe045ede8f2a4
Richard Liang <152163506+fearlesslyh@users.noreply.github.com>
2024-12-15 21:19:44 +0800

    delete

--- NewMiniRPC/src/main/java/com/lyh/rpc/registry/RegistryCache.java
+++ NewMiniRPC/src/main/java/com/lyh/rpc/registry/RegistryCache.java
@@ -1,9 +1,16 @@
 package com.lyh.rpc.registry;
 
+import com.lyh.rpc.model.ServiceInfoDefine;
+
+import java.util.List;
+
 /**
  * @author 梁懿豪
  * @version 1.3
  */
 public class RegistryCache {
+    List<ServiceInfoDefine>serviceCache;
+    void add(){
 
+    }
 }
